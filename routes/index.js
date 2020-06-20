ff91a14955ad262c6168214e6bf75d3cd629de30
Milton Câmara <miltoncamaragomes@gmail.com>
2020-06-20 08:20:13 -0300

    Adicionado variavel de ambiente VAR1

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index',
+    {
+      title: 'Express',
+      var1: process.env.Var1
+    });
 });
 
 module.exports = router;
